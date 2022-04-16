67dbe5a36635099808fad7a7476ec81fd1cd763a
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2022-04-16 04:24:05 +0300

    Create Project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '@utils/createEmotionCache';
+import { appWithTranslation } from 'next-i18next';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import GeneralProvider from '@src/providers/GeneralProvider';
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
+    
+  return (
+    <CacheProvider value={emotionCache}>
+      <GeneralProvider>   
+        <Component {...pageProps} />
+      </GeneralProvider>
+    </CacheProvider>
+  );
+
+}
+
+export default appWithTranslation(MyApp);

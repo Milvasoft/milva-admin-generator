456e64d6047a69e72980cba01d878cd00b2ff9e1
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2022-04-19 09:15:19 +0300

    PhoneNumberInput

---
+++ src/components/PhoneNumberInput.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React from 'react';
+import {
+  FormControl, 
+  Input,
+  InputLabel,
+  InputProps 
+} from '@mui/material';
+import { IMaskInput } from 'react-imask';
+
+interface CustomProps {
+    onChange: (event: { target: { name: string; value: string } }) => void;
+    name: string;
+  }
+  
+const TextMaskCustom = React.forwardRef<HTMLElement, CustomProps>(
+  function TextMaskCustom(props, ref) {
+
+    const { onChange, ...other } = props;
+
+    return (
+      <IMaskInput
+        {...other}
+        mask="0 (000) 000 00 00"
+        definitions={{ '#': /[1-9]/, }}
+        // @ts-ignore
+        inputRef={ref}
+        onAccept={(value: any) => onChange({ target: { name: props.name, value } })}
+        overwrite
+      />
+    );
+  
+  },
+);
+
+interface IPhoneNumberInput extends InputProps{
+    label?: string
+}
+  
+export default function PhoneNumberInput(params: IPhoneNumberInput) {
+
+  return (
+    <FormControl variant="outlined">
+
+      <InputLabel htmlFor="formatted-text-mask-input">{params?.label}</InputLabel>
+
+      <Input {...params} inputComponent={TextMaskCustom as any} />
+
+    </FormControl>
+  );
+
+}

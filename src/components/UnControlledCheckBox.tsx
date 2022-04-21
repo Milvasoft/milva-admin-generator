fcf8d3192e040733011f04a3317cac424727ffe8
Ali Burhan Keskin <32386753+aliburhankeskin@users.noreply.github.com>
2022-04-22 02:27:03 +0300

    UnControlledCheckBox

--- src/components/UnControlledCheckBox.tsx
+++ src/components/UnControlledCheckBox.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/jsx-key */
-import { Checkbox, FormControlLabel, FormGroup } from '@mui/material';
+import {
+  Checkbox, 
+  FormControl, 
+  FormControlLabel, 
+  FormGroup,
+} from '@mui/material';
 import React, { memo } from 'react';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import OutlinedWrapper from './OutlinedWrapper';
 
 type params = {
   data: Array<{
@@ -10,29 +16,34 @@ type params = {
     name?: string,
     defaultChecked?: boolean
   }>,
-  register?: any
+  register?: any,
+  title?: string,
 }
 
-export default memo(function UnControlledCheckBox({ data, register }: params) {
+export default memo(function UnControlledCheckBox({ data, register, title }: params) {
 
   return (
-    <FormGroup row sx={{ alignSelf: 'flex-start', }}>
-      {React.Children.toArray(data.map(
-        (element) => (
-          <FormControlLabel
-            label={element.label}
-            control={(
-              <Checkbox              
-                {...register(element?.name)}
-                defaultChecked={element.defaultChecked}
-                icon={<CheckBoxOutlineBlankIcon />}
-                checkedIcon={<CheckBoxIcon />}
+    <OutlinedWrapper label={title}> 
+      <FormControl fullWidth>    
+        <FormGroup row sx={{ alignSelf: 'flex-start', }}>
+          {React.Children.toArray(data.map(
+            (element) => (
+              <FormControlLabel
+                label={element.label}
+                control={(
+                  <Checkbox              
+                    {...register(element?.name)}
+                    defaultChecked={element.defaultChecked}
+                    icon={<CheckBoxOutlineBlankIcon />}
+                    checkedIcon={<CheckBoxIcon />}
+                  />
+                )}
               />
-            )}
-          />
-        )
-      ))}
-    </FormGroup>
+            )
+          ))}
+        </FormGroup>
+      </FormControl>
+    </OutlinedWrapper>
   );
 
 });
